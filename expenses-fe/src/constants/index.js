1185761e150dc093bb86eca934cc6ba41f6b8da6
Samuele Mattiuzzo <samuele.mattiuzzo@madetech.com>
2023-10-02 12:38:11 +0100

    Small updates

--- expenses-fe/src/constants/index.js
+++ expenses-fe/src/constants/index.js
@@ -1,1 +1,1 @@
-export const API_URL = "http://localhost:8000/api/categories/";
+export const API_URL = "http://127.0.0.1:8000/api/expenses/";
--- expenses/admin.py
+++ expenses/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from expenses.models import Category, Expense
 
 # Register your models here.
+admin.site.register(Category)
+admin.site.register(Expense)
